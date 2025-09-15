b72a53f381788f831aa8449ee9255315f71b52e4
mukarrom <mukarrom21@gmail.com>
2025-09-15 09:26:53 +0600

    feat: implement CRUD operations for student management in controller and service

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
 import { StudentService } from './student.service';
 import { Student } from './student.schema';
 
@@ -9,5 +9,36 @@ export class StudentController {
     @Post()
     async addStudent(@Body() data: Partial<Student>) {
         return this.studentService.createStudent(data);
+    }
+
+    @Get()
+    async getStudents() {
+        return this.studentService.getAllStudents();
+    }
+
+    @Get(':id')
+    async getStudentById(@Param('id') id: string) {
+        return this.studentService.getStudentById(id);
+    }
+
+    @Put(':id')
+    async updateStudent(
+        @Param('id') id: string,
+        @Body() data: Partial<Student>,
+    ) {
+        return this.studentService.updateStudentCompletely(id, data);
+    }
+
+    @Patch(':id')
+    async patchStudent(
+        @Param('id') id: string,
+        @Body() data: Partial<Student>,
+    ) {
+        return this.studentService.UpdateStudentPartially(id, data);
+    }
+
+    @Delete(':id')
+    async deleteStudent(@Param('id') id: string) {
+        return this.studentService.deleteStudent(id);
     }
 }
--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -13,4 +13,33 @@ export class StudentService {
         const newStudent = new this.studentModel(data);
         return newStudent.save();
     }
+
+    // Fetch all students
+    async getAllStudents(): Promise<Student[]> {
+        return this.studentModel.find().exec();
+    }
+
+    // Fetch a student by ID
+    async getStudentById(id: string): Promise<Student | null> {
+        return this.studentModel.findById(id).exec();
+    }
+
+    // Update a student by ID
+    async updateStudentCompletely(id: string, data: Partial<Student>): Promise<Student | null> {
+        // return this.studentModel.findByIdAndUpdate(id, data, { new: true }).exec();
+        return this.studentModel.findByIdAndUpdate(id, {
+            name: data.name ?? null,
+            age: data.age ?? null,
+            email: data.email ?? null,
+        }, { overwrite: true, new: true }).exec();
+    }
+    // Update a student by ID
+    async UpdateStudentPartially(id: string, data: Partial<Student>): Promise<Student | null> {
+        return this.studentModel.findByIdAndUpdate(id, data, { new: true }).exec();
+    }
+
+    // Delete a student by ID
+    async deleteStudent(id: string): Promise<Student | null> {
+        return this.studentModel.findByIdAndDelete(id).exec();
+    }
 }
